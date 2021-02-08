13842241557ea6a67e5b6c2453d1160a12a1df34
Ajit Singh Rawat <47390463+AJIT-SINGH-RAWAT@users.noreply.github.com>
2021-02-08 19:09:31 +0530

    Create ques-2.cpp

---
+++ ques-2.cpp
@@ -0,0 +1,3 @@
+I have a doubt in second question like if we have to consecutive days are missing then how we can calculate average ??
+let says if we have monday and tuesday is missing then for calculating monday we need value of sunday and tuesday 
+but we have only the value of sunday we cannot calculate it
